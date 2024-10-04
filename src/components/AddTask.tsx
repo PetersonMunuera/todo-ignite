0330d2bc4950acd0af904335a1a6b8703f4b2a1c
Peterson Munuera <peterson.munuera@gmail.com>
2024-10-04 16:12:19 -0300

    feat: handling add task

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,12 +1,37 @@
+import { useState, ChangeEvent, FormEvent } from 'react'
+
 import { PlusCircle } from '@phosphor-icons/react'
 import styles from './AddTask.module.css'
 
 export function AddTask() {
-  return (
-    <form className={styles.container}>
-      <input className={styles.input} type="text" placeholder="Adicione uma nova tarefa" />
+  const [taskInput, setTaskInput] = useState("")
 
-      <button className={styles.button}>
+  function handleTaskInput(event: ChangeEvent<HTMLInputElement>) {
+    const task = event.target.value
+    setTaskInput(task)
+  }
+
+  function handleAddTask(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    if (!taskInput) return
+
+    console.log(`Adicionando a tarefa ${taskInput} na lista de tarefas`)
+
+    setTaskInput("")
+  }
+
+  return (
+    <form onSubmit={handleAddTask} className={styles.container}>
+      <input 
+        className={styles.input} 
+        type="text" 
+        placeholder="Adicione uma nova tarefa"
+        onChange={handleTaskInput}
+        value={taskInput}
+      />
+
+      <button className={styles.button} disabled={!taskInput}>
         <span>Criar</span>
         <PlusCircle size={20} />
       </button>
