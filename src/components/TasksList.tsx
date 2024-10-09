0cdc659749f373b26c99e1649616f29502a62b95
Peterson Munuera <peterson.munuera@gmail.com>
2024-10-09 16:50:54 -0300

    feat: empty list and counters

--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -1,5 +1,6 @@
 import { Circle, Trash } from '@phosphor-icons/react'
 
+import clipboardImg from '../assets/clipboard.svg'
 import styles from './TasksList.module.css'
 
 interface TasksListProps {
@@ -13,31 +14,41 @@ export function TasksList({ tasks, onRemoveTask }: TasksListProps) {
       <header className={styles.header}>
         <div className={styles.createdTasks}>
           <strong>Tarefas criadas</strong>
-          <span>5</span>
+          <span>{tasks.length}</span>
         </div>
 
         <div className={styles.completedTasks}>
           <strong>Concluídas</strong>
-          <span>2 de 5</span>
+          <span>{tasks.length ? `2 de ${tasks.length}` : 0}</span>
         </div>
       </header>
 
-      <ul className={styles.list}>
-        {tasks.map(task => {
-          return (
-            <li key={task} className={styles.taskItem}>
-              <Circle size={20} color='#4EA8DE' />
-              <span>{task}</span>
-              <button 
-                onClick={() => onRemoveTask(task)} 
-                className={styles.removeButton}
-              >
-                <Trash size={20} />
-              </button>
-            </li>
-          )
-        })}
-      </ul>
+      {tasks.length ? (
+        <ul className={styles.list}>
+          {tasks.map(task => {
+            return (
+              <li key={task} className={styles.taskItem}>
+                <Circle size={20} color='#4EA8DE' />
+                <span>{task}</span>
+                <button
+                  onClick={() => onRemoveTask(task)}
+                  className={styles.removeButton}
+                >
+                  <Trash size={20} />
+                </button>
+              </li>
+            )
+          })}
+        </ul>
+      ) : (
+        <div className={styles.emptyList}>
+          <img src={clipboardImg} alt="Lista vazia" />
+          <p>
+            <strong>Você ainda não tem tarefas cadastradas</strong>
+            Crie tarefas e organize seus itens a fazer
+          </p>
+        </div>
+      )}
     </main>
   )
 }
